11355921bf390884ac6bf7689e2f1c3f1c5bd63b
srihariash999 <srihariash@gmail.com>
2022-01-26 17:42:19 +0530

    fixed profile get api

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,11 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const auth = require('../middleware/auth');
 
 
-router.get('/me', async (req, res) => {
+router.get('/me', auth, async (req, res) => {
+   
     const user = await User.findById(req.user._id).select('-password');
     res.send(user);
 });
