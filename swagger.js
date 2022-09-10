76843be243521a50e5f2d1a4c753d99431ea74df
Srihari Ayapilla <srihariash@gmail.com>
2022-09-10 18:35:24 +0530

    updated swagger base url

--- swagger.js
+++ swagger.js
@@ -1,25 +1,23 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
-    info: {
-        title: 'Moneyboi API',
-        description: 'Moneyboi is an expense tracker app',
+  info: {
+    title: "Moneyboi API",
+    description: "Moneyboi is an expense tracker app",
+  },
+  host: "zealous-gold-lab-coat.cyclic.app",
+  schemes: ["https"],
+  definitions: {
+    AddUser: {
+      $email: "user@email.com",
+      $name: "John Doe",
+      password: "",
     },
-    host: 'localhost:3000',
-    schemes: ['http'],
-    definitions: {
-        AddUser: {
-            $email: "user@email.com",
-            $name: "John Doe",
-            password: ""
-        }
-    }
+  },
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./index.js'];
-
-
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./index.js"];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
